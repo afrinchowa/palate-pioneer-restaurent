2d956c3cdb294194947dbec9fca3132b59178512
Chowa <arfinchowa524@gmail.com>
2024-04-16 23:28:32 +0600

    firebase set up

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBPVATrJP3Ewuu7bAM_ghOtqqfxQJ2tNOs",
+  authDomain: "palate-pioneer-54ab1.firebaseapp.com",
+  projectId: "palate-pioneer-54ab1",
+  storageBucket: "palate-pioneer-54ab1.appspot.com",
+  messagingSenderId: "119638650374",
+  appId: "1:119638650374:web:61dfcbd666fffd96965d87"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
