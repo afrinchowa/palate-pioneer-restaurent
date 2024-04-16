497fe3e7154d8db5052da214419b56420bfeca04
Chowa <arfinchowa524@gmail.com>
2024-04-16 23:20:49 +0600

    login page event handled

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,13 +1,21 @@
 const Login = () => {
+
+    const handleLogin = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email,password);
+    }
   return (
-    <div className="hero min-h-screen w-1/2 bg-base-200">
-      <div className="hero-content flex-col ">
-        <div className="text-center lg:text-left">
+    <div className="hero min-h-screen  bg-base-200">
+      <div className="hero-content flex-col md:flex-row-reverse">
+        <div className="text-center md:w-1/2 lg:text-left">
           <h1 className="text-5xl font-bold">Login now!</h1>
           
         </div>
-        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form className="card-body">
+        <div className="card md:w-1/2 max-w-sm shadow-2xl bg-base-100">
+          <form onSubmit={handleLogin} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
