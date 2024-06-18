c0acb0b0cfe608c3ae7965209436aafc08db036d
Chowa <arfinchowa524@gmail.com>
2024-06-18 22:08:12 +0600

    JWT setup

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -10,6 +10,7 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
@@ -17,8 +18,8 @@ const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
-const googleProvider = new GoogleAuthProvider()
-
+  const googleProvider = new GoogleAuthProvider();
+  const axiosPublic = useAxiosPublic();
 
   const createUser = (email, password) => {
     setLoading(true);
@@ -26,34 +27,43 @@ const googleProvider = new GoogleAuthProvider()
   };
   const signIn = (email, password) => {
     setLoading(true);
-    return signInWithEmailAndPassword(auth,email, password);
+    return signInWithEmailAndPassword(auth, email, password);
   };
 
-const googleSignIn =() => {
-  setLoading(true);
-  return signInWithPopup(auth,googleProvider)
-}
+  const googleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
 
   const logOut = () => {
     setLoading(true);
     return signOut(auth);
   };
 
-const updateUserProfile=(name,photo) =>{
- return updateProfile(auth.currentUser,{
-  displayName:name, photoURL:photo
-})
-}
+  const updateUserProfile = (name, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
-     if(currentUser){
-// get token and  store client 
-     }
-     else{
-      // TODO: remove token (if token stored in the client side:Local storage , caching,in memory )
-     }
+      if (currentUser) {
+        // get token and  store client
+        const userInfo = { email: currentUser.email };
+        axiosPublic.post("/jwt", userInfo)
+        .then((res) => {
+          if (res.data.token) {
+            localStorage.setItem("access-token", res.data.token);
+          }
+        });
+      } else {
+        // TODO: remove token (if token stored in the client side:Local storage , caching,in memory )
+        localStorage.removeItem("access-token");
+        
+      }
       setLoading(false);
     });
     return () => {
@@ -68,7 +78,7 @@ const updateUserProfile=(name,photo) =>{
     signIn,
     googleSignIn,
     logOut,
-    updateUserProfile
+    updateUserProfile,
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
