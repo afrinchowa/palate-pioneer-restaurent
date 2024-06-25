c474009568c2b6f915eb8619126314c7a770a260
Chowa <arfinchowa524@gmail.com>
2024-06-25 23:07:10 +0600

    payment start

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,17 +1,26 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
 
+
+// add publishable key
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 
 const Payment = () => {
-    return (
-        <div>
-            <SectionTitle 
-            heading="Payment" subHeading="You wanna Pay To Eat"
-            ></SectionTitle>
-            <div className="text-2xl">
-
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <SectionTitle
+        heading="Payment"
+        subHeading="You wanna Pay To Eat"
+      ></SectionTitle>
+      <div className="text-2xl">
+        <Elements stripe={stripePromise}>
+          <CheckoutForm></CheckoutForm>
+        </Elements>
+      </div>
+    </div>
+  );
 };
 
 export default Payment;
